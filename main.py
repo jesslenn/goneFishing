f61153ba6ab824e33b8b79ea97af3af7e2cda159
jesslenn <jessica.lennick@gmail.com>
2023-11-05 10:19:52 -0500

    sample animation from pygame

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+# Example file showing a circle moving on screen
+import pygame
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
 
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.draw.circle(screen, "red", player_pos, 40)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
